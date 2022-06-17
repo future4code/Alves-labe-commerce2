18d545b49d2922ba293a9273e22e3ec22195095e
Henrique Dos Santos Rodriguez <henrique_rz@hotmail.com>
2022-06-17 11:01:47 -0300

    Continuando

--- src/Components/PartePrinicipal/PartePrinicipal.js
+++ src/Components/PartePrinicipal/PartePrinicipal.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const ContainerCarrinho = styled.div `
+border: 2px solid black;
+display: flex;
+flex-direction: column;
+align-items: center;
+box-shadow: 3px 3px 3px 2px #0e3364;
+transition: all .2s ease-in-out;
+&:hover{
+    top:-4px;box-shadow:3px 4px 3px  #999;
+}
+h3 {
+    margin-bottom: -5px;
+    color: white;
+    text-align: center;
+}
+p {
+    color: white;
+}
+button {
+    margin-bottom: 5px;
+    border-radius: 10px;
+    border: none;
+    &:hover{
+        background-color: #0c5e96;
+        color: white;
+    }
+}
+`
 
 export default class PartePrinicipal extends Component {
+
   render() {
     return (
-      <div>PartePrinicipal</div>
+        <ContainerCarrinho>
+        <div>
+          
+        </div>
+          <h3>Carrinho</h3>
+        </ContainerCarrinho>
     )
   }
 }
